ff4a1c28573b38f55dcb57bbbda07d67999bef8b
liangshan <1763123933@qq.com>
2021-02-24 23:47:41 +0800

    tank   根据按键改变tank方向

---
+++ Tank01/src/Dir.java
@@ -0,0 +1,9 @@
+/**
+ * @author liangshan
+ * @ClassName Dir
+ * @description
+ * @date 2021/2/24 23:40
+ **/
+public enum Dir {
+    RIGHT,LEFT,UP,DOWN;
+}
--- Tank01/src/TankFrame.java
+++ Tank01/src/TankFrame.java
@@ -13,6 +13,8 @@ import java.awt.event.WindowEvent;
 public class TankFrame extends Frame {
     int x = 200;
     int y = 200;
+    Dir dir = Dir.DOWN;
+    private final int speed = 10;
     public TankFrame(){
         this.setResizable(true);//可调整大小
         this.setSize(800,600);//窗口大小
@@ -32,16 +34,70 @@ public class TankFrame extends Frame {
     }
 
     class MyKeyListener extends KeyAdapter{
+        //定义tank移动方向
+        boolean bl = false;
+        boolean br = false;
+        boolean bu = false;
+        boolean bd = false;
 
         @Override
         public void keyPressed(KeyEvent e) {
-            x+=10;
+            int keyCode = e.getKeyCode();
+            switch (keyCode){
+                case KeyEvent.VK_LEFT:
+                    bl = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    br = true;
+                    break;
+                case KeyEvent.VK_UP:
+                    bu = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bd = true;
+                    break;
+                default:
+                    break;
+            }
+            setTankDirection();
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
-            y+=20;
-            System.out.println("key released");
+            int keyCode = e.getKeyCode();
+            switch (keyCode) {
+                case KeyEvent.VK_LEFT:
+                    bl = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    br = false;
+                    break;
+                case KeyEvent.VK_UP:
+                    bu = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bd = false;
+                    break;
+                default:
+                    break;
+            }
+            setTankDirection();
+        }
+
+        private void setTankDirection(){
+            if(bl){
+                dir = Dir.LEFT;
+            }
+            if(br){
+                dir = Dir.RIGHT;
+            }
+            if(bu){
+                dir = Dir.UP;
+            }
+            if(bd){
+                dir = Dir.DOWN;
+            }
+
         }
     }
 
@@ -49,6 +105,20 @@ public class TankFrame extends Frame {
     @Override
     public void paint(Graphics g){
         g.fillRect(x,y,50,50);
+        switch (dir) {
+            case LEFT:
+                x-=speed;
+                break;
+            case RIGHT:
+                x+=speed;
+                break;
+            case UP:
+                y-=speed;
+                break;
+            case DOWN:
+                y+=speed;
+                break;
+        }
     }
 
 }
